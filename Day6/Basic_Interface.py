80ad7462b2936446f203ae5e83dd24efbd8cff7e
SrinivasThuppudu <srinivasthuppudu@gmail.com>
2025-01-30 16:21:44 +0530

    Add files via upload

---
+++ Day6/Basic_Interface.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    
+    @abstractmethod
+    def make_sound(self):
+        pass
+    
+class Dog(Animal):
+    
+    def make_sound(self):
+        print("Dog barks")
+
+dog = Dog()
+dog.make_sound()
+# Output: Dog barks
